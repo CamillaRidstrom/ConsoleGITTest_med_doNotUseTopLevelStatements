b23ffd465fd246d31c2396105a146a4ff482115e
Elev Lexicon <Elev Lexicon@DESKTOP-LC5CLFU>
2023-09-12 14:23:07 +0200

    Add project files.

---
+++ ConsoleGITTest_med_doNotUseTopLevelStatements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleGITTest_med_doNotUseTopLevelStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
